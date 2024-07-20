23e9efa686fff41bfeb99b375ae044a19845ead6
Navin-kumar2502 <114897504+Navin-kumar2502@users.noreply.github.com>
2024-07-20 21:09:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [tsconfigPaths(),react()],    
+  plugins: [tsconfigPaths({ root: __dirname }), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          antd: ["antd"],
+        },
+      },
+    },
+  },
 });
